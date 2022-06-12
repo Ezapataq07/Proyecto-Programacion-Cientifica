9a740b7ab3dd2e2dfeea3bc8cf1542e59e505e67
Ezapataq07 <ezapataq@unal.edu.co>
2022-06-12 08:29:27 -0500

    Cambios a las ecuaciones como funciones. Falta distinguir entre T2 y T4

--- Julián/ecuacionesConstitutivas.py
+++ Julián/ecuacionesConstitutivas.py
@@ -1,7 +1,8 @@
 import numpy as np
 import math
+from Emanuel.RK4 import *
 fm1 = 10 #Flujo masico
-fm3 = 10 # Flujo masico
+fm2 = 10 # Flujo masico
 epsilon = 10
 gB = 10 #Grados Brix
 NBf=10
@@ -12,21 +13,21 @@ KTtb = 10 #Conductividad termica
 Dexttb = 10
 TeA = 10
 TeJ = 10
-T2=10
-T4=10
+
 
 wtb = 10 #Espesor del tubos
 
 Cpj = 4.187*(1 - 0.006*gB) #Capacidad calorifica especifica del jugo.
 
-T = 273.15 #Grados Kelvin.
+#T = 273.15 #Grados Kelvin.
 
-Cpa = 7.2951406*(10**(-8))*(T**3) - 7.431358269*(10**(-5))*(T**2) + 0.02620653*T + 1.0156045
+def Cpa(T):
+    return 7.2951406*(10**(-8))*(T**3) - 7.431358269*(10**(-5))*(T**2) + 0.02620653*T + 1.0156045
 #Capacidad calorifica del agua.
-
-pj = 1179.7 - 0.354*T  #Densidad del jugo.
-
-pA = -0.004323394923*((T-273.15)**2) - 0.04038343824*(T-273.15) + 1000.807908 #Densidad del agua.
+def pj(T):
+    return 1179.7 - 0.354*T  #Densidad del jugo.
+def pA(T):
+    return -0.004323394923*((T-273.15)**2) - 0.04038343824*(T-273.15) + 1000.807908 #Densidad del agua.
 
 Ntb = 10 #Número de tubos.
 Ltb = 10 #Longitud de los tubos.
@@ -42,48 +43,87 @@ Dexttb = 10 #Diametro externo de los tubos.
 
 VA = Ntb*Lcz*((np.pi*(Dintcz**2))/4) - Ntb*Ltb*((np.pi*(Dexttb**2))/4)
 
-Mj = pj*Vj #Masa del jugo.
+def Mj(T):
+    return pj(T)*Vj #Masa del jugo.
 
-MA = pA*VA #Masa del agua.
+def MA(T):
+    return pA(T)*VA #Masa del agua.
 
 Mtb = 10 #Masa del tubo.
 Mcz = 10 #Masa de la coraza.
 kTA = 10
 
-Hj = 5.46829932*(10**(-4))*((T-273.15)**2) + 3.694618471*(T-273.15)-0.633984 #Entalpia del jugo.
+def Hj(T):
+    return 5.46829932*(10**(-4))*((T-273.15)**2) + 3.694618471*(T-273.15)-0.633984 #Entalpia del jugo.
 Deqcz = 4((((Pitb**2)*math.sqrt(3))/4) - np.pi*(Dexttb**2))/((np.pi*Dexttb)/2)
-muA = (0.0002471250997*(T-273.15)**2)-0.03226252622(T-273.15)+1.545370351;
-muJ = (0.047)/(T-273.15)
-NReA = (pA*Deqcz*VA)/(muA)
-AFtb = (np.pi*(Dinttb**2)/(4))
-velJ = (fm1/pj)/(AFtb)
 
-NReJ = (pj*Vj*Dinttb)/muJ
-NPrA = (Cpa*muA)/(kTA)
-hA = (kTJ*Deqcz)*(0.36*(NReA)**(0.55)*NPrA**(0.333))
-HA = 4.180358722*(T-273.15) + 0.3506658477 #Entalpia del agua.
+def muA(T):
+    return (0.0002471250997*(T-273.15)**2)-0.03226252622(T-273.15)+1.545370351
+def muJ(T):
+    return (0.047)/(T-273.15)
+
+def NReA(T):
+    return (pA(T)*Deqcz*VA)/(muA(T))
+AFtb = (np.pi*(Dinttb**2)/(4))
+
+def velJ(T):
+    return (fm1/pj(T))/(AFtb)
+
+def NReJ(T):
+    return (pj(T)*Vj*Dinttb)/muJ(T)
+
+def NPrA(T):
+    return (Cpa(T)*muA(T))/(kTA)
+
+def hA(T):
+    return (kTJ*Deqcz)*(0.36*(NReA(T))**(0.55)*(NPrA(T))**(0.333))
+
+def HA(T):
+    return 4.180358722*(T-273.15) + 0.3506658477 #Entalpia del agua.
 AHtbJ = np.pi*Dinttb*Ltb
 AHtbA = np.pi*Dexttb*Ltb
 AH = (1/2)*(AHtbJ+ AHtbA)
-deltaT1 = TeJ - T4
-deltaT2 = T2 - TeA
 
-deltaTML = (deltaT1-deltaT2)/(math.log(deltaT1/deltaT2))
+def deltaT1(T4):
+    return TeJ - T4
+
+def deltaT2(T2):
+    return T2 - TeA
+
+def deltaTML(T2,T4):
+    return (deltaT1(T4)-deltaT2(T2))/(math.log(deltaT1(T4)/deltaT2(T2)))
+
 AHtb = (AHtbJ + AHtbA)/2
-NPrj = (Cpj*muJ)/(kTJ)
-hJ = (kTJ/Dinttb)*(0.0243*(NReJ)**(0.8)*NPrj*0.333)
-U = 1/(AH*((1/(hJ*AHtbJ))+(wtb/(KTtb*AHtb))+(1/(hA*AHtbA))))
-dQIdeC = U*AH*deltaTML
+
+def NPrj(T):
+    return (Cpj*muJ(T))/(kTJ)
+
+def hJ(T):
+    return (kTJ/Dinttb)*(0.0243*(NReJ(T))**(0.8)*(NPrj(T))*0.333)
+
+def U(T):
+    return 1/(AH*((1/(hJ(T)*AHtbJ))+(wtb/(KTtb*AHtb))+(1/(hA(T)*AHtbA))))
+
+def dQIdeC(T,T2,T4):
+    return U(T)*AH*deltaTML(T2,T4)
+
 ##dQtbA_A = hA*AHtbA*(TtbA-TA); 
-dQtbA_A = dQIdeC;
+
+def dQtbA_A(T,T2,T4):
+    return dQIdeC(T,T2,T4)
+
 
 
 
 
 
 ##dQJ_tbJ = hJ*AHtbJ*(TJ-TtbJ);
-dQJ_tbJ = dQIdeC
-dQtbJ_tbA = dQJ_tbJ - dQtbA_A
+def dQJ_tbJ(T,T2,T4):
+    return dQIdeC(T,T2,T4)
+
+def dQtbJ_tbA(T,T2,T4):
+    return dQJ_tbJ(T,T2,T4) - dQtbA_A(T,T2,T4)
+
 
 dQA_czA = 0
 
@@ -91,11 +131,27 @@ dQczA_czat = 0 #Presuntamente todo esto es igual a 0.
 
 dQtbJ_tbA = 0
 
-NNu = (hJ*Dinttb)/(kTJ)
+def NNu(T):
+    return (hJ(T)*Dinttb)/(kTJ)
+
 
 Septb = Pitb - Dexttb
 AFcz = (Dintcz*Septb*SepBf)/Pitb
-fD = (-2*math.log10((epsilon/Dinttb)-(5.02/NReJ)*(math.log10((epsilon/(Dinttb*3.71)+(14.5/NReJ))))))**(-2) #que es epsilon?
-hf1_2=fD*((Ltb*(velJ)**2)/(Dinttb*2));
-hfcz=fD*(Dintcz*(NBf+1))/(Deqcz);
-vAcz = ((fm1/pA)/AFcz)
+
+def fD(T):
+    return (-2*math.log10((epsilon/Dinttb)-(5.02/NReJ(T))*(math.log10((epsilon/(Dinttb*3.71)+(14.5/NReJ(T)))))))**(-2) #que es epsilon?
+
+def hf1_2(T):
+    return fD(T)*((Ltb*(velJ(T))**2)/(Dinttb*2))
+
+def hfcz(T):
+    return fD(T)*(Dintcz*(NBf+1))/(Deqcz)
+
+def vAcz(T):
+    return ((fm1/pA(T))/AFcz)
+
+
+############################################################
+T1 = 10
+def fT2(t, T2):
+    return (1/(Cpj*Mj(T2)))*(fm1*Hj(T1)-fm2*Hj(T2)-dQJ_tbJ(T2,T2,T4))
